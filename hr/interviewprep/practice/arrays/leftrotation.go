c537fdf81bc97086df2adf83079e053792732fb5
Nicholas Sladic <nicholasgsladic@gmail.com>
2020-03-20 14:29:55 -0400

    Update leftrotation.go

--- hr/interviewprep/practice/arrays/leftrotation.go
+++ hr/interviewprep/practice/arrays/leftrotation.go
@@ -20,23 +20,3 @@ func main() {
 
 	fmt.Println(rotLeft(arr, d))
 }
-
-// func leftRotation(a []int, size int, rotation int) []int {
-
-// 	var newArray []int
-// 	for i := 0; i < rotation; i++ {
-// 		newArray = a[1:size]
-// 		fmt.Println(newArray)
-// 		newArray = append(newArray, a[0])
-// 		// fmt.Println(newArray)
-// 		a = newArray
-// 	}
-// 	return a
-// }
-
-// func main() {
-// 	a := []int{1, 2, 3, 4, 5}
-// 	fmt.Printf("input: %+v\n", a)
-// 	rotation := 4
-// 	fmt.Printf("output: %+v\n", leftRotation(a, len(a), rotation))
-// }
